0c46f885831bb9979050dd6703e2b911dde8b63b
Himaja <himajamailavarapu6@gmail.com>
2026-06-13 14:39:44 +0530

    Added array rotation programs

---
+++ Arrays/LeftRotateArray.java
@@ -0,0 +1,21 @@
+public class LeftRotateArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int first = arr[0];
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        arr[arr.length - 1] = first;
+
+        System.out.print("Left Rotated Array: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
