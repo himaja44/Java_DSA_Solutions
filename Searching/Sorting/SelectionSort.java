4469bc46a84a43a49a08a7cd0c3b8eaa58593206
Himaja <himajamailavarapu6@gmail.com>
2026-06-22 13:37:54 +0530

    Added LongestWord string program

---
+++ Searching/Sorting/SelectionSort.java
@@ -0,0 +1,30 @@
+package Sorting;
+public class SelectionSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {64, 25, 12, 22, 11};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int min = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.print("Sorted Array: ");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
