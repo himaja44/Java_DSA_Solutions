79cbd8d1d3d2d5dd41541a6a88548c54fecf00b8
Himaja <himajamailavarapu6@gmail.com>
2026-06-29 13:42:16 +0530

    Added OOP inheritance and encapsulation examples

--- Arrays/MergeTwoArrays.java
+++ Arrays/MergeTwoArrays.java
@@ -1,3 +1,4 @@
+package Arrays;
 public class MergeTwoArrays {
 
     public static void main(String[] args) {
---
+++ Patterns/OOP/MultilevelInheritance.java
@@ -0,0 +1,34 @@
+package OOP;
+
+class Animal {
+
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Puppy extends Dog {
+
+    void weep() {
+        System.out.println("Puppy is weeping");
+    }
+}
+
+public class MultilevelInheritance {
+
+    public static void main(String[] args) {
+
+        Puppy p = new Puppy();
+
+        p.eat();
+        p.bark();
+        p.weep();
+    }
+}
