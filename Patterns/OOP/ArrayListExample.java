510b6073bf636d5ac9b169231e3bee3eef04fc2c
Himaja <himajamailavarapu6@gmail.com>
2026-07-02 12:09:03 +0530

    Added Java Collections examples

---
+++ Patterns/OOP/ArrayListExample.java
@@ -0,0 +1,20 @@
+package Patterns.OOP;
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+        list.add("Python");
+        list.add("C++");
+
+        System.out.println("ArrayList Elements:");
+
+        for (String language : list) {
+            System.out.println(language);
+        }
+    }
+}
