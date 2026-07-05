be6fe5b0b042e5bc1b339c5c885069db8d059749
Himaja <himajamailavarapu6@gmail.com>
2026-07-05 14:22:11 +0530

    Added recursion examples

---
+++ Recursions/FactorialUsingRecursion.java
@@ -0,0 +1,20 @@
+package Recursions;
+
+
+public class FactorialUsingRecursion {
+
+    static int factorial(int n) {
+
+        if (n == 0 || n == 1)
+            return 1;
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        int num = 5;
+
+        System.out.println("Factorial = " + factorial(num));
+    }
+}
