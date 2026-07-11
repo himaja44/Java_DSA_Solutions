26f38a79f8ff98bb5282f51d57d93ba9cd72758b
Himaja <himajamailavarapu6@gmail.com>
2026-07-11 16:07:34 +0530

    Added recursion example programs

---
+++ Recursions/PowerOfNumber.java
@@ -0,0 +1,15 @@
+public class PowerOfNumber {
+
+    static int power(int base, int exponent) {
+
+        if (exponent == 0)
+            return 1;
+
+        return base * power(base, exponent - 1);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Result: " + power(2, 5));
+    }
+}
