ea24a2bae92e829877d10556ecbef30a0db365a9
Himaja <himajamailavarapu6@gmail.com>
2026-07-15 14:21:38 +0530

    Added advanced pattern programs

---
+++ Patterns/ZigZagPattern.java
@@ -0,0 +1,21 @@
+public class ZigZagPattern {
+
+    public static void main(String[] args) {
+
+        int rows = 3;
+        int cols = 9;
+
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= cols; j++) {
+
+                if ((i + j) % 4 == 0 || (i == 2 && j % 4 == 0))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
