5ec1a4d191982883970fa2edece32b2eb0c5dddf
Himaja <himajamailavarapu6@gmail.com>
2026-08-07 10:52:57 +0530

    Added advanced linked list operations

---
+++ LinkedLists/RemoveDuplicates.java
@@ -0,0 +1,38 @@
+package LinkedLists;
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(20);
+        head.next.next.next = new Node(30);
+
+        Node temp = head;
+
+        while (temp != null && temp.next != null) {
+
+            if (temp.data == temp.next.data) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+
+        temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
