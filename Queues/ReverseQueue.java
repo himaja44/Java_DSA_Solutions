8f1b615a3557dbca457b16fed1e3f4e58f2d41b1
Himaja <himajamailavarapu6@gmail.com>
2026-08-27 13:07:14 +0530

    Added more queue practice programs

---
+++ Queues/ReverseQueue.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class ReverseQueue {
+
+    public static void main(String[] args) {
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+
+        Stack<Integer> stack = new Stack<>();
+
+        while (!queue.isEmpty()) {
+            stack.push(queue.remove());
+        }
+
+        while (!stack.isEmpty()) {
+            queue.add(stack.pop());
+        }
+
+        System.out.println("Reversed Queue: " + queue);
+    }
+}
