cba2a2719d74b306fb82562b0b0c3d0f71cf6b0a
Himaja <himajamailavarapu6@gmail.com>
2026-08-28 12:43:31 +0530

    Added circular queue programs

---
+++ Queues/CircularQueue.java
@@ -0,0 +1,49 @@
+public class CircularQueue {
+
+    static int[] queue = new int[5];
+    static int front = -1;
+    static int rear = -1;
+
+    static void enqueue(int value) {
+
+        if ((rear + 1) % queue.length == front) {
+            System.out.println("Queue is Full");
+            return;
+        }
+
+        if (front == -1) {
+            front = 0;
+        }
+
+        rear = (rear + 1) % queue.length;
+        queue[rear] = value;
+    }
+
+    static void display() {
+
+        if (front == -1) {
+            System.out.println("Queue is Empty");
+            return;
+        }
+
+        int i = front;
+
+        do {
+            System.out.print(queue[i] + " ");
+            i = (i + 1) % queue.length;
+        } while (i != (rear + 1) % queue.length);
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        enqueue(10);
+        enqueue(20);
+        enqueue(30);
+        enqueue(40);
+
+        System.out.println("Circular Queue:");
+        display();
+    }
+}
