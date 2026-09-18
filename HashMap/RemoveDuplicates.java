5d7b72a36c85fec16535c5fe3a3bab72ce988a84
Himaja <himajamailavarapu6@gmail.com>
2026-09-18 11:53:47 +0530

    Added HashSet practice programs

---
+++ HashMap/RemoveDuplicates.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 10, 30, 20, 40};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        System.out.println("Unique Elements: " + set);
+    }
+}
