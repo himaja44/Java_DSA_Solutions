dccb9a1b6dff4a887357a0709362b9d64272421c
Himaja <himajamailavarapu6@gmail.com>
2026-09-23 18:04:25 +0530

    Added HashSet practice programs

---
+++ HashMap/UnionOfArrays.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+
+public class UnionOfArrays {
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = {3, 4, 5, 6};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : arr1) {
+            set.add(num);
+        }
+
+        for (int num : arr2) {
+            set.add(num);
+        }
+
+        System.out.println("Union: " + set);
+    }
+}
